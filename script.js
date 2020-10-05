de83594eb2e565a4bf7cfe4800bb328049c1a39b
gersondani2001-prog <gersondani2001@gmail.com>
2020-10-05 12:12:19 -0600

    Modals

---
+++ script.js
@@ -0,0 +1,21 @@
+// codigo fuente de javascripts logica del sitio web
+
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    event.target.style.display = "none";
+  }
+}
